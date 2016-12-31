f7e168d261e2a34a20cc1d97303bfffad4216d33
Marc Fieser <midfies@gmail.com>
2016-12-30 19:35:40 -0800

    working through testing

--- learning_journal/views/default.py
+++ learning_journal/views/default.py
@@ -1,7 +1,10 @@
+"""Default views for learning journal web app."""
+
 from pyramid.response import Response
 from pyramid.view import view_config
 from pyramid.httpexceptions import HTTPFound
 from sqlalchemy.exc import DBAPIError
+from pyramid.view import notfound_view_config
 
 from ..models import Entry
 import datetime
@@ -32,14 +35,28 @@ def create_view(request):
         new_title = request.POST["title"]
         new_body = request.POST["body"]
         new_date = datetime.datetime.now().date()
-        new_category = request.POST["category"]
+        new_category = request.POST["category"].title().replace(" ", "")
         new_tags = request.POST["tags"]
         new_entry = Entry(title=new_title, body=new_body, creation_date=new_date, category=new_category, tags=new_tags)
-        
+
         request.dbsession.add(new_entry)
 
         return HTTPFound(location='/')
-    return {} 
+    return {}
+
+
+@view_config(route_name="category", renderer="../templates/category.jinja2")
+def category_view(request):
+    """View for post of different categories."""
+    query = request.dbsession.query(Entry)
+    entries = query.filter(Entry.category == request.matchdict['category']).order_by(Entry.creation_date.desc()).all()
+    return {"entries": entries}
+
+
+@view_config(route_name="about", renderer="../templates/about.jinja2")
+def about_view(request):
+    """View for about me."""
+    return {}
 
 
 db_err_msg = """\
